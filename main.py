3e20de93c633640ce9c40f3a7ef0ab8528675961
Himmelt <master@void-3.cn>
2023-05-23 10:00:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aligo import Aligo
 name = 'XXMixReal-v3.0.safetensors'
 url = 'https://civitai.com/api/download/models/77903'
 
-runcmd("wget -O XXMixReal-v3.0.safetensors https://civitai.com/api/download/models/77903", verbose = True)
+runcmd("wget --no-check-certificate -O XXMixReal-v3.0.safetensors https://civitai.com/api/download/models/77903", verbose = True)
 
 ali = Aligo()
 remote_folder = ali.get_folder_by_path('AI绘画/SD_模型')
